d10a5d5aaa394238a871f2c1dfcd23ae35b6abc2
juicyjake12 <108505695+juicyjake12@users.noreply.github.com>
2022-08-11 21:42:05 -0400

    updated files

--- js/javascript.js
+++ js/javascript.js
@@ -1,12 +1,72 @@
 const playbtn = document.getElementById("playbtn");
 const highscorebtn = document.getElementById("highscorebtn");
+const questionContainer = document.getElementById("question-container")
+
+let currentQuestion = {}
+let acceptingAnswers = true
+let highscore = 0
+let availableQuestions = []
 
 
+const questions = [
+    {
+        question: "what are strings",
+        choice1: "2",
+        choice2: "3",
+        choice3: "3",
+        answer: 2,
 
+    },
+]
+question: [
+    {
+        question: "what are strings",
+        choice1: "2",
+        choice2: "3",
+        choice3: "3",
+        answer: 2,
 
+    },
+]
+question: [
+    {
+        question: "what are strings",
+        choice1: "2",
+        choice2: "3",
+        choice3: "3",
+        answer: 2,
 
-//questions to be asked
+    },
+]
+question: [
+    {
+        question: "what are strings",
+        choice1: "2",
+        choice2: "3",
+        choice3: "3",
+        answer: 2,
 
+    },
+]
+question: [
+    {
+        question: "what are strings",
+        choice1: "2",
+        choice2: "3",
+        choice3: "3",
+        answer: 2,
 
+    }
+]
+const score = 100
+const maxquestions = 5
+
+playbtn.addEventListener("click", startgame)
+
+function startgame()  {
+    console.log("started")
+    playbtn.classList.add("hide")
+    getNewQuestion()
+}
 
 
