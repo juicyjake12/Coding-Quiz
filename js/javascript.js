9a768f2f39d93dc034e908cb30c0e005db48332e
juicyjake12 <108505695+juicyjake12@users.noreply.github.com>
2022-08-16 02:12:15 -0400

    updated javascript

--- js/javascript.js
+++ js/javascript.js
@@ -1,72 +1,39 @@
 const playbtn = document.getElementById("playbtn");
-const highscorebtn = document.getElementById("highscorebtn");
+const HighScoresbtn = document.getElementById("HighScoresbtn");
 const questionContainer = document.getElementById("question-container")
 
-let currentQuestion = {}
-let acceptingAnswers = true
-let highscore = 0
-let availableQuestions = []
 
 
-const questions = [
-    {
-        question: "what are strings",
-        choice1: "2",
-        choice2: "3",
-        choice3: "3",
-        answer: 2,
 
-    },
-]
-question: [
-    {
-        question: "what are strings",
-        choice1: "2",
-        choice2: "3",
-        choice3: "3",
-        answer: 2,
+var Questions = [
+    "in an infinite loop, a loop statment never ends because its conditional expression is never truthy",
+    "JavaScript can be used to delete persistent cookies in a program",
+    " 'var' is used to set a variable",
+    "getElementbyId is used to get a class from the html",
+    "classList.add creates a id",
+    "An Array can store multiple values using a special syntax",
+    //dont forget quizover
+];
 
-    },
-]
-question: [
-    {
-        question: "what are strings",
-        choice1: "2",
-        choice2: "3",
-        choice3: "3",
-        answer: 2,
+var questionCounter = 1;
+var finalScore = 6;
 
-    },
-]
-question: [
-    {
-        question: "what are strings",
-        choice1: "2",
-        choice2: "3",
-        choice3: "3",
-        answer: 2,
+playbtn.addEventListener("click", startgame);
+playbtn.addEventListener("click", createbutton)
 
-    },
-]
-question: [
-    {
-        question: "what are strings",
-        choice1: "2",
-        choice2: "3",
-        choice3: "3",
-        answer: 2,
 
-    }
-]
-const score = 100
-const maxquestions = 5
-
-playbtn.addEventListener("click", startgame)
-
-function startgame()  {
-    console.log("started")
-    playbtn.classList.add("hide")
-    getNewQuestion()
+function startgame() {
+    console.log("started");
+    setnextQuestion();
 }
+function setnextQuestion() {
+    document.getElementById("question-container")
+     $("#question-container").html(Questions[0]);
+    questionCounter++
+
+}
+
+
+
 
 
